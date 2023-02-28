e562e149fc54281636847c9a80357916169f02d5
EC2 Default User <ec2-user@ip-172-31-33-54.ap-northeast-1.compute.internal>
2023-02-28 07:50:38 +0000

    [質問用]Bookers1(仮)

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  get "/" => "homes#top"
+  resources :books
+  #post 'books' => 'books#create'
+  #get 'books' => 'books#index'
+  #get 'books/:id' => 'books#show', as: 'book'
+  #et 'books/:id/edit' => 'books#edit', as: 'edit_book'
+  #patch 'books/:id' => 'books#update', as: 'update_book'
+  #delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
